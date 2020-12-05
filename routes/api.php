197e7e38f04ed904aa9433791de6b47a532d54f7
Zan <zan19.jeric@gmail.com>
2020-12-05 09:29:13 +0100

    Edit API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('categories', CategoryController::class)->except(['create','edit']);
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::resource('categories', CategoryController::class)->except(['create','edit']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
