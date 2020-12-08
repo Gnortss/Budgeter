a6115fa86905ca54269b068565f43d57cb3769e4
Jure Casar <jure.casar1@gmail.com>
2020-12-08 15:04:46 +0100

    special offer api not working yet

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CouponController;
+use App\Http\Controllers\SpecialOfferController;
 use App\Http\Controllers\AuthController;
 
 /*
@@ -24,6 +25,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::resource('categories', CategoryController::class)->except(['create','edit']);
     Route::resource('coupons', CouponController::class);
+    Route::resource('offers', SpecialOfferController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
